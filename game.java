05dec9d02dc3a248114dd9547db0a4113c5e96dc
Carlos-Muniz <carlos.muniz@rutgers.edu>
2020-07-02 18:21:37 -0400

    Working on IO

--- game.java
+++ game.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
+
 
 class Game {
     public static int startInput() {
         System.out.println("Welcome to Hangman!");
-        Scanner objectInput = new Scanner(System.in);
+        Scanner  objectInput = new Scanner(System.in);
         System.out.println("Think of a word and enter the number of letters in that word");
 
         String wordSizeString = objectInput.nextLine();
@@ -15,11 +21,29 @@ class Game {
         return wordSize;
     }
 
-    public static void constrainDatabase(int wordSize) {
-        
+    public static void loadData(int wordSize) {
+        String path = "words.csv";
+
+        File tmpDir = new File(path);
+        boolean exists = tmpDir.exists();
+        System.out.println("File Exists: " + exists);
+
+        // BufferedReader csvReader = new BufferedReader(new FileReader(path));
+        // List<String> words = new ArrayList<String>();
+        // String line;
+        // while ((line = csvReader.readLine()) != null) {
+        //     String[] row_data = line.split(",");
+        //     int size = Integer.parseInt(row_data[2]);
+        //     if (size == wordSize) {
+        //         words.add(row_data[1]);
+        //     }
+        // }
+        // System.out.println("Words List Size is: " + words.size());
+        // csvReader.close();
     }
 
     public static void main(String [] args) {
         int wordSize = startInput();
+        loadData(wordSize);
     }
 }
