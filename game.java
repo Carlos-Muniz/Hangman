e4c23a34fbf91bcc2826da3c2d28c004de1e1189
Carlos-Muniz <carlos.muniz@rutgers.edu>
2020-07-06 16:54:18 -0400

    Made some progress on the input. All that is left is the constraining process.

--- game.java
+++ game.java
@@ -14,12 +14,12 @@ import java.util.ArrayList;
 class Game {
     public static int startInput() {
         System.out.println("Welcome to Hangman!");
-        final Scanner objectInput = new Scanner(System.in);
-        System.out.println("Think of a word and enter the number of letters in that word");
+        Scanner objectInput = new Scanner(System.in);
+        System.out.print("Think of a word and enter the number of letters in that word: ");
 
-        final String wordSizeString = objectInput.nextLine();
-        final int wordSize = Integer.parseInt(wordSizeString);
+        int wordSize = objectInput.nextInt();
         System.out.println("Word Size is: " + wordSize);
+        objectInput.nextLine();
 
         objectInput.close();
 
@@ -82,30 +82,28 @@ class Game {
 
 
     public static void guessWords(int wordSize, List<String> words) {
+        Scanner ansInput = new Scanner(System.in);
         String mess = "The rules are simple. I'll guess a letter that is in the word. " +
         " If I'm right, you tell me 'yes', and where that letter is. " +
         "If I'm wrong you tell me 'no'. I have 5 guesses to get it right.";
         System.out.println(mess);
         char[] letters = new char[wordSize];
         Arrays.fill(letters, '?');
-        String indices = "";
+        String indices = " ";
         String space = "  ";
         for (int i = 0; i < wordSize; i++) {
             if (i+1 > 9) {
-                space = " ";
+                space = "+";
             }
-            indices = indices + space + String.valueOf(i+1) + " ";
+            indices = indices + space + String.valueOf(i+1) + "+";
         }
 
         int count = 5;
-        Scanner ansInput = new Scanner(System.in);
+       
         while (count > 0) {
         // 1) Guess letter
             char guess = guessLetter(words);
-            // System.out.println("My guess: " + guess);
-            // while (!ansInput.hasNextLine()) {
-            //     ansInput.nextLine();
-            // }
+            System.out.println("My guess: " + guess);
             // 2) Accept input for yes or no
             System.out.println("Did I guess right? (yes or no)");
             String answer = ansInput.nextLine();
@@ -124,11 +122,71 @@ class Game {
             // 5) Repeat
     }
 
+    public static void whilePractice() {
+        int count = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("The Count is: " + count);
+        while (count < 10) {
+            String answer = scan.nextLine();
+            System.out.println(answer + count);
+            count++;
+        }
+        scan.close();
+    }
+
     public static void main(final String[] args) {
-        final int wordSize = startInput();
-        final List<String> wordSelection = loadData(wordSize);
+        // final int wordSize = startInput();
+        // final List<String> wordSelection = loadData(wordSize);
         // char guess = guessLetter(wordSelection);
         // System.out.println("Guess: " + guess);
-        guessWords(wordSize, wordSelection);
+        // guessWords(wordSize, wordSelection);
+        
+        System.out.println("Welcome to Hangman!");
+        Scanner objectInput = new Scanner(System.in);
+        System.out.print("Think of a word and enter the number of letters in that word: ");
+        int wordSize = objectInput.nextInt();
+        objectInput.nextLine();
+
+        final List<String> wordSelection = loadData(wordSize);
+
+        String mess = "The rules are simple. I'll guess a letter that is in the word. " +
+        " If I'm right, you tell me 'yes', and where that letter is. " +
+        "If I'm wrong you tell me 'no'. I have 5 guesses to get it right.";
+        System.out.println(mess);
+        char[] letters = new char[wordSize];
+        Arrays.fill(letters, '_');
+        String indices = " ";
+        String space = "  ";
+        for (int i = 0; i < wordSize; i++) {
+            if (i+1 > 9) {
+                space = " ";
+            }
+            indices = indices + space + String.valueOf(i+1) + " ";
+        }
+
+        int count = 5;
+        while (count > 0) {
+        // 1) Guess letter
+            char guess = guessLetter(wordSelection);
+            System.out.println("My guess: " + guess);
+            
+            // 2) Accept input for yes or no
+            System.out.println("Did I guess right? (yes or no)");
+            String answer = objectInput.nextLine();
+
+            if (answer.equals("yes")){
+                System.out.println(String.valueOf(letters).replace("", "   "));
+                System.out.println(indices);
+                System.out.println("Where does the letter go? (Use spaces to separate multiple answers.)");
+                String[] places = objectInput.nextLine().split(" ");
+                int[] placeArray = new int[places.length];
+                for(int i = 0; i < placeArray.length; i ++) {
+                    placeArray[i] = Integer.parseInt(places[i]);
+                }
+
+            }
+        }
+        objectInput.close();
+
     }
 }
