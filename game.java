3ae2307153d35830c0a7dfa7498bccbd7e40903c
Carlos-Muniz <carlos.muniz@rutgers.edu>
2020-07-07 10:21:51 -0400

    Fully works. Now I just need to add comments and update the readme!

--- game.java
+++ game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Set;
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,28 +7,29 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
 class Game {
-    public static int startInput() {
+    public static int startInput(Scanner objectInput) {
+
         System.out.println("Welcome to Hangman!");
-        Scanner objectInput = new Scanner(System.in);
         System.out.print("Think of a word and enter the number of letters in that word: ");
-
-        int wordSize = objectInput.nextInt();
-        System.out.println("Word Size is: " + wordSize);
+        final int wordSize = objectInput.nextInt();
         objectInput.nextLine();
-
-        objectInput.close();
 
         return wordSize;
     }
 
+
     public static List<String> loadData(final int wordSize) {
-        String path = "words.csv";
+        final String path = "words.csv";
 
         BufferedReader br = null;
         List<String> words = null;
@@ -37,32 +39,31 @@ class Game {
             words = new ArrayList<String>();
             String line;
             while ((line = br.readLine()) != null) {
-                String[] rowData = line.split(",");
-                int size = Integer.parseInt(rowData[2]);
+                final String[] rowData = line.split(",");
+                final int size = Integer.parseInt(rowData[2]);
                 if (size == wordSize) {
                     words.add(rowData[1]);
                 }
             }
-            // System.out.println("Words List Size is: " + words.size());
             br.close();
-        } catch (FileNotFoundException e) {
+        } catch (final FileNotFoundException e) {
             e.printStackTrace();
-            File tmpDir = new File(path);
-            boolean exists = tmpDir.exists();
+            final File tmpDir = new File(path);
+            final boolean exists = tmpDir.exists();
             System.out.println("File Exists: " + exists);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
         return words;
     }
 
 
-    public static char guessLetter(List<String> words) {
+    public static char guessLetter(final List<String> words, final Set<Character> letters) {
         char c = ' ';
-        Map<Character,Integer> charCount = new HashMap<Character,Integer>();
+        final Map<Character, Integer> charCount = new HashMap<Character, Integer>();
         for (int i = 0; i < words.size(); i++) {
-            for (int j=0; j < words.get(i).length(); j++) {
-                char ch = words.get(i).charAt(j);
+            for (int j = 0; j < words.get(i).length(); j++) {
+                final char ch = words.get(i).charAt(j);
                 if (!charCount.containsKey(ch)) {
                     charCount.put(ch, 0);
                 }
@@ -70,9 +71,9 @@ class Game {
             }
         }
         int max = 0;
-        for (char key : charCount.keySet()) {
-            int val = charCount.get(key);
-            if (val > max) {
+        for (final char key : charCount.keySet()) {
+            final int val = charCount.get(key);
+            if ((val > max) & (!letters.contains(key))) {
                 max = val;
                 c = key;
             }
@@ -81,111 +82,104 @@ class Game {
     }
 
 
-    public static void guessWords(int wordSize, List<String> words) {
-        Scanner ansInput = new Scanner(System.in);
-        String mess = "The rules are simple. I'll guess a letter that is in the word. " +
-        " If I'm right, you tell me 'yes', and where that letter is. " +
-        "If I'm wrong you tell me 'no'. I have 5 guesses to get it right.";
-        System.out.println(mess);
-        char[] letters = new char[wordSize];
-        Arrays.fill(letters, '?');
-        String indices = " ";
-        String space = "  ";
-        for (int i = 0; i < wordSize; i++) {
-            if (i+1 > 9) {
-                space = "+";
+    public static void constrainDataset(final List<String> words, final String[] places, final char letter,
+            final int wordSize) {
+
+        final String[] regexPattern = new String[wordSize];
+        final String neg = "[^" + String.valueOf(letter) + "]";
+        Arrays.fill(regexPattern, neg);
+
+        for (int i = 0; i < places.length; i++) {
+            final int j = Integer.parseInt(places[i]) - 1;
+            if (j >= 0) {
+                regexPattern[j] = String.valueOf(letter);
             }
-            indices = indices + space + String.valueOf(i+1) + "+";
         }
 
-        int count = 5;
-       
-        while (count > 0) {
-        // 1) Guess letter
-            char guess = guessLetter(words);
-            System.out.println("My guess: " + guess);
-            // 2) Accept input for yes or no
-            System.out.println("Did I guess right? (yes or no)");
-            String answer = ansInput.nextLine();
-            System.out.println(answer);
-            
-            // if (answer == "yes"){
-            //     System.out.println(letters.toString().replace("", "  "));
-            //     System.out.println(indices);
-            //     System.out.println("Where does the letter go? (Use spaces to separate multiple answers.)");
-            //     String places = ansInput.nextLine()
+        final Pattern pattern = Pattern.compile(String.join("", regexPattern));
+        Matcher matcher;
+        for (final Iterator<String> iter = words.listIterator(); iter.hasNext();) {
+            matcher = pattern.matcher(iter.next());
+            if (!matcher.find()) {
+                iter.remove();
+            }
         }
-        ansInput.close();
-
-            // 3) If yes, accept input for locations, else continue
-            // 4) Constrain dataset.
-            // 5) Repeat
     }
 
-    public static void whilePractice() {
-        int count = 0;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("The Count is: " + count);
-        while (count < 10) {
-            String answer = scan.nextLine();
-            System.out.println(answer + count);
-            count++;
-        }
-        scan.close();
-    }
 
-    public static void main(final String[] args) {
-        // final int wordSize = startInput();
-        // final List<String> wordSelection = loadData(wordSize);
-        // char guess = guessLetter(wordSelection);
-        // System.out.println("Guess: " + guess);
-        // guessWords(wordSize, wordSelection);
+    public static void guessWords(int wordSize, List<String> wordSelection, Scanner objectInput) {
+        final String mess = "The rules are simple. I'll guess a letter that is in the word. "
+            + " If I'm right, you tell me 'yes', and where that letter is. "
+            + "If I'm wrong you tell me 'no'. I have 5 guesses to get it right.";
         
-        System.out.println("Welcome to Hangman!");
-        Scanner objectInput = new Scanner(System.in);
-        System.out.print("Think of a word and enter the number of letters in that word: ");
-        int wordSize = objectInput.nextInt();
-        objectInput.nextLine();
-
-        final List<String> wordSelection = loadData(wordSize);
-
-        String mess = "The rules are simple. I'll guess a letter that is in the word. " +
-        " If I'm right, you tell me 'yes', and where that letter is. " +
-        "If I'm wrong you tell me 'no'. I have 5 guesses to get it right.";
         System.out.println(mess);
-        char[] letters = new char[wordSize];
+        final char[] letters = new char[wordSize];
         Arrays.fill(letters, '_');
         String indices = " ";
         String space = "  ";
         for (int i = 0; i < wordSize; i++) {
-            if (i+1 > 9) {
+            if (i + 1 > 9) {
                 space = " ";
             }
-            indices = indices + space + String.valueOf(i+1) + " ";
+            indices = indices + space + String.valueOf(i + 1) + " ";
+        }
+
+        final Set<Character> lettersPicked = new HashSet<Character>();
+        for (final char c : letters) {
+            lettersPicked.add(c);
         }
 
         int count = 5;
-        while (count > 0) {
-        // 1) Guess letter
-            char guess = guessLetter(wordSelection);
+        boolean wordIncomplete = true;
+        while ((count > 0) & (wordIncomplete)) {
+            final char guess = guessLetter(wordSelection, lettersPicked);
             System.out.println("My guess: " + guess);
-            
-            // 2) Accept input for yes or no
-            System.out.println("Did I guess right? (yes or no)");
-            String answer = objectInput.nextLine();
 
-            if (answer.equals("yes")){
+            System.out.println("Did I guess right? (yes or no)");
+            final String answer = objectInput.nextLine();
+            String[] places = { "0" };
+            if (answer.equals("yes")) {
                 System.out.println(String.valueOf(letters).replace("", "   "));
                 System.out.println(indices);
-                System.out.println("Where does the letter go? (Use spaces to separate multiple answers.)");
-                String[] places = objectInput.nextLine().split(" ");
-                int[] placeArray = new int[places.length];
-                for(int i = 0; i < placeArray.length; i ++) {
-                    placeArray[i] = Integer.parseInt(places[i]);
+                System.out.println("Where does the letter go? (Use commas (,) to separate multiple answers.)");
+                places = objectInput.nextLine().split(",");
+                for (int i = 0; i < places.length; i++) {
+                    final int j = Integer.parseInt(places[i]) - 1;
+                    letters[j] = guess;
                 }
-
+                System.out.println(String.valueOf(letters).replace("", "   "));
+                count++;
+            }
+            constrainDataset(wordSelection, places, guess, wordSize);
+            lettersPicked.add(guess);
+            count--;
+            wordIncomplete = false;
+            for (int i=0; i < letters.length; i ++) {
+                if (letters[i] == '_') {
+                    wordIncomplete = true;
+                }
             }
         }
+
+        if (count == 0) {
+            System.out.println("I lost...");
+        }
+        if (!wordIncomplete) {
+            System.out.println("I won! The word is: " + String.valueOf(letters));
+        }
+        System.out.println("Good Game!");
+    }
+
+
+    public static void main(final String[] args) {
+
+        Scanner objectInput = new Scanner(System.in);
+        int wordSize = startInput(objectInput);
+
+        final List<String> wordSelection = loadData(wordSize);
+
+        guessWords(wordSize, wordSelection, objectInput);
+
         objectInput.close();
 
     }
